d4ac9460fee3ded6c0fe61f29d0fe357e5d31f2e
beorbusy <beorbusy@gmail.com>
2024-10-20 21:08:39 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,1 +1,4 @@
-pip install fuzzy_wuzzy
+!pip install Flask transformers sentence-transformers fuzzywuzzy
+!python chatbot_flask.py
+!pip install pyngrok
+!ngrok authtoken 2mt5Y7GYuQLj95iSCgDqLbrgH5X_78TzbfhXN7a7somF9RvE9
